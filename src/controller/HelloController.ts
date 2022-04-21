66921c2f303ec521a2f8f3163dbbd0c0698e976f
damoga89 <david.dmg89@gmail.com>
2022-04-21 10:56:30 +0200

    HelloController & HelloRouter & RootRouter done

---
+++ src/controller/HelloController.ts
@@ -0,0 +1,17 @@
+import { BasicResponse } from "./types";
+import { IHelloController } from "./interfaces";
+import { LogSucces } from "../utils/logger";
+
+export class HelloController implements IHelloController{
+    
+    public async getMessage(name?: string): Promise<BasicResponse> {
+        LogSucces('[/api/hello] Get Request');
+
+        return {
+            message: `Hello, ${name || "world!"}`
+        }
+    }
+}
+
+
+
